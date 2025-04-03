cec133be325087cc06bb51d959e3749498d459b0
Tenley Gorman <tgorman2@tulane.edu>
2025-04-03 12:03:25 -0500

    added a button
    button test

--- apptest1deploy.py
+++ apptest1deploy.py
@@ -4,3 +4,12 @@ st.title("My first forrealskies streamlit app")
 st.write("Howdyyyy!")
 
 st.write("trying to make a change and check on github desktop")
+
+#make a click me button 
+if st.button("Click me!"):
+    st.write("You clicked me!")
+    st.balloons()
+else:
+    st.write("Click the button to see what happens!")
+
+        
